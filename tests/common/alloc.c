4994d6b3a1dc700317f3cbc7babd077e39af10aa
Dowland Aiello <dowlandaiello@gmail.com>
2020-09-15 17:04:52 -0400

    tests: Finish test for alloc.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- tests/common/alloc.c
+++ tests/common/alloc.c
@@ -1,6 +1,23 @@
+#include <stdio.h>
+
 #include "common/alloc.c"
 
 int main()
 {
+    // Make strings to fit up to 100 chars
+    for (int i = 0; i < 100; i++) {
+        // Make a string to fit 10 characters
+        struct String my_string = make_string(i);
+
+        // Force the string to allocate more space
+        for (int j = 0; j <= i; j++) {
+            push_string(&my_string, 'a');
+        }
+
+        printf("%s\n", my_string.contents);
+
+        destroy_string(&my_string);
+    }
+
     return 0;
 }
