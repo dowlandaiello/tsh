5a4f932971d60a2fb772b3b570ae688a65f334e8
Dowland Aiello <dowlandaiello@gmail.com>
2020-09-16 21:19:06 -0400

    tsh: Format tests folder.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- tests/cmd.c
+++ tests/cmd.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 #include "cmd.h"
 
 int main()
 {
-        struct ArgList args = make_arg_list();
+    struct ArgList args = make_arg_list();
 
-        for (int i = 0; i < 69; i++) {
-                struct String arg = make_string(1);
-                push_string(&arg, 'a');
+    for (int i = 0; i < 69; i++) {
+        struct String arg = make_string(1);
+        push_string(&arg, 'a');
 
-                push_arg_arg_list(&args, arg);
-        }
+        push_arg_arg_list(&args, arg);
+    }
 
-        for (int i = 0; i < args.length; i++) {
-                printf("%s ", args.args[i].contents);
-        }
+    for (int i = 0; i < args.length; i++) {
+        printf("%s ", args.args[i].contents);
+    }
 
-        printf("\n");
+    printf("\n");
 
-        assert(args.length == 69);
+    assert(args.length == 69);
 
-        destroy_arg_list(&args);
+    destroy_arg_list(&args);
 
-        return 0;
+    return 0;
 }
